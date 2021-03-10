d578cc2cb263ceff508026195774d9c9f5e2bd0d
Elias Vincent Peters <eliasvpeters@gmail.com>
2021-03-10 03:23:25 +0100

    put index in public

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
-require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 use App\Core\Http\Request;
 use App\Core\Foundation\Application;
 
-$app = Application::run(__DIR__);
+$basePath = __DIR__ . '/..'; // fix this
+
+$app = Application::run($basePath);
 
 $kernel = $app->makeKernel();
 $kernel->handle($request = Request::capture());
